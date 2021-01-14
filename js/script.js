bc9e5051520eceb92a6833b5e013eb130eef0e74
Nicola <nicolusso@gmail.com>
2021-01-14 15:26:26 +0100

    Generatore 5 num senza duplicati

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
 // Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che
 // sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+var numeriGenerati = [];
+
+$( document ).ready(function() {
+
+  // Generare e stampare 5 numeri random senza duplicati
+  while (numeriGenerati.length < 5) {
+  var numeroCasuale = numeroRandom(1, 100);
+
+  if (numeriGenerati.includes(numeroCasuale) == false) {
+    numeriGenerati.push(numeroCasuale);
+  }
+
+}
+  console.log(numeriGenerati);
+  alert("Riuscirai a ricordati questi numeri?\n\n"  + '     ' + numeriGenerati);
+
+
+});
 
 
 // Funzione Numero Random
