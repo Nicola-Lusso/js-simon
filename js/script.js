0cd56e015cc4d26f77372a50f6612f50ab96c86a
Nicola <nicolusso@gmail.com>
2021-01-14 15:10:25 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che
+// sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+// Funzione Numero Random
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
